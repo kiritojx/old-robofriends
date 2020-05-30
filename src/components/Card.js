b21d23315cbe3066c8188ec5f4d16e32b0b0015f
Phu Le Ngo <30664920+kiritojx@users.noreply.github.com>
2020-05-31 01:39:01 +0700

    Update master branch

---
+++ src/components/Card.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Card = (props) => {
+
+    // Destructering
+    const {id, name, email} = props;
+    return (
+        <div className="bg-light-green dib grow ma2 br3 pad3 shadow-5 tc">
+            <img alt='avatar-robots' src={`https://robohash.org/${id}?size=200x200`} />
+            <div>
+                <h2>
+                    {name}
+                </h2>
+                <p>
+                    {email}
+                </p>
+            </div>
+        </div>
+
+    );
+}
+
+export default Card;
---
+++ src/components/CardList.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Card from './Card'
+
+const CardList = ({ robots }) => {
+    // if (true) {
+    //     throw new Error('Noooooooo!');
+    // }
+    return (
+        <div>
+            {
+                robots.map((name, index) => {
+                    return (<Card
+                        key={index}
+                        id={robots[index].id}
+                        name={robots[index].name}
+                        email={robots[index].email}
+
+                    />
+                    );
+                })
+            }
+        </div>
+    );
+}
+
+export default CardList;
