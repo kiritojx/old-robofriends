e06195583571914e5fbb0ccf448e8453cccd38b6
Phu Le Ngo <phulengo@Phus-MacBook-Pro.local>
2020-06-17 00:26:07 +0700

    Updating version package

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Card = (props) => {
 
-    // Destructering
-    const {id, name, email} = props;
+    // Destructuring
+    const { id, name, email } = props;
     return (
         <div className="bg-light-green dib grow ma2 br3 pad3 shadow-5 tc">
             <img alt='avatar-robots' src={`https://robohash.org/${id}?size=200x200`} />
