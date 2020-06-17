03b116f76dfb7c0d8e7601b7593a8d1596b5342d
kiritojx <quainhanp24@gmail.com>
2020-06-17 10:04:37 +0700

    Redux Async Function

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,42 +4,44 @@ import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import ErrorBoundary from '../components/ErrorBoundary';
-import { setSearchField } from '../actions';
+import { setSearchField, requestRobots } from '../actions';
 // import { render } from '@testing-library/react';
 
 const mapStateToProps = state => {
     return {
-        searchField: state.searchField
+        searchField: state.searchRobots.searchField,
+        robots: state.requestRobots.robots,
+        isPending: state.requestRobots.isPending,
+        error: state.requestRobots.error
     }
 }
 
 const mapDispatchToProps = (dispatch) => {
     return {
-        onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+        onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+        onRequestRobots: () => dispatch(requestRobots())
     }
 }
 
 class App extends Component {
-    constructor() {
-        super();
-        this.state = {
-            robots: []
-        }
-    }
+    // constructor() {
+    //     super();
+    //     this.state = {
+    //         robots: []
+    //     }
+    // }
 
     componentDidMount() {
-        fetch('https://jsonplaceholder.typicode.com/users')
-            .then(response => response.json())
-            .then(users => this.setState({ robots: users }));
+        this.props.onRequestRobots();
     }
 
     render = () => {
-        const { robots } = this.state; // Destructering
-        const { searchField, onSearchChange } = this.props;
+        // const { robots } = this.state; // Destructuring
+        const { searchField, onSearchChange, robots, isPending } = this.props;
         const filterRobots = robots.filter(robot => {
             return robot.name.toLowerCase().includes(searchField.toLowerCase());
         })
-        return !robots.length ?
+        return isPending ?
             <h1>Loading...</h1> :
 
             <div className="tc">
